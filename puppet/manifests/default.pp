cc6114057a3f533559db138853a819bf0b344e7d
Justin York <justincyork@gmail.com>
2013-08-07 15:01:18 -0600

    Install rvm, ruby, and a gemset

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,3 +1,16 @@
 # Puppet manifest
 
-# include apt
+include rvm
+
+rvm_system_ruby {
+  '1.9.3':
+    ensure => 'present',
+    default_use => true;
+}
+
+rvm_gemset {
+  '1.9.3@myopicvicar':
+    ensure => present,
+    require => Rvm_system_ruby['1.9.3'];
+}
+
